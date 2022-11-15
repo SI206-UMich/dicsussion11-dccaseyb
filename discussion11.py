9c0112fbe383057deaa4d6915748097ff4a690d3
Casey Bressler <cbressler@MacBook-Air-472.local>
2022-11-15 16:50:53 -0500

    first commit

--- discussion11.py
+++ discussion11.py
@@ -39,12 +39,16 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    #THe above line deletes everything and restarts, not always necessary
+    cur.execute("CREATE TABLE Patients (pet_id INTEGER PRIMARY KEY, name TEXT, species_id INTEGER, age INTEGER, cuteness INTEGER, aggressiveness INTEGER)") 
+    conn.commit()
 
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (?,?,?,?,?,?)",("0", "Fluffle", "0", "3", "90", "100"))
+    conn.commit()
     
 
 # TASK 2
@@ -59,13 +63,29 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
+    species = ["Rabbit",
+    "Dog",
+    "Cat",
+    "Boa Constrictor",
+    "Chinchilla",
+    "Hamster",
+    "Cobra",
+    "Parrot",
+    "Shark",
+    "Goldfish",
+    "Gerbil",
+    "Llama",
+    "Hare"
+    ]
+    for i in range(len(json_data)):
+        cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (?,?,?,?,?,?)",(i + 1, json_data[i]['name'], species.index(json_data[i]['species']), json_data[i]['age'], json_data[i]['cuteness'], json_data[i]['aggressiveness']))
+    conn.commit()
 
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
-    pass
+    
 
 
 
